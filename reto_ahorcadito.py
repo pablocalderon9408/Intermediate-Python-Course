1d116fd3fb8fe098443655bbb51bce07f4bd2f58
pablocalderon9408 <pablocalderon9408@gmail.com>
2021-06-18 12:48:52 -0500

    LOGROS: *Limpiar pantalla. * Palabra aleatoria de lista.

--- reto_ahorcadito.py
+++ reto_ahorcadito.py
@@ -1,6 +1,31 @@
+import os
+from random import randint
+
+
+
+def archivo():
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        words = [line for line in f]
+        # print(words)
+
+    choosen_word = words[randint(0,len(words))]
+    print(choosen_word)
+    print(len(choosen_word)* "-")
+
+
+
+
 def run():
+
+    os.system("clear")
+
+    print("""
+                        ¡¡¡¡JUEGO DEL AHORCADO!!!
+    ------------------------------------------------------------------------------
+    """)
+    letra = input("Ingresa una letra: ")
     pass
 
 if __name__ == "__main__":
-    run()
+    archivo()
 

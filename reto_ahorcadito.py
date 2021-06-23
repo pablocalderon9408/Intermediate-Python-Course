587a1bf65468e06bafa08482791cc5c61fab5aab
pablocalderon9408 <pablocalderon9408@gmail.com>
2021-06-23 08:08:56 -0500

    Terminé el juego. Logré hacer que la palabra se fuera actualizando y no quedara como una lista

--- reto_ahorcadito.py
+++ reto_ahorcadito.py
@@ -18,36 +18,44 @@ def run():
     ****************************************************************************
     Se te muestra una cadena de texto de modo que sepas cuantos carácteres tiene la palabra que debes adivinar. Tienes X intentos
     """)
-    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
-        words = [line for line in f]
+    with open("./archivos/data.txt", "r", encoding="utf-8") as data:
+        words = [line for line in data]
         # print(words)
     choosen_word = words[randint(0,len(words))]
     print(choosen_word)
     # print(len(choosen_word))
     word_in_code = [ " _ " for i in range(0, len(choosen_word)-1)]
     # word_in_code =  [(len(choosen_word)- 1) * "-"]
-    print((len(choosen_word)- 1) * " _ ")
+    #print((len(choosen_word)- 1) * " _ ")
     # print(word_in_code)
     x = True 
-    word_in_list = ""
+    #word_in_list = ""
     
 
     while x == True:
 
+        for element in word_in_code:
+            print(element +" ",end="")
+        print("\n")
+
         letter = input("Ingresa una letra: ").lower()
         for i in range(0,len(choosen_word)):
             if choosen_word[i] == letter:
+                #print(word_in_code[i])
                 word_in_code[i] = letter
+
+        #print(word_in_code)
 
         # for lett in word_in_code:
         #     word_in_list += lett
 
-        print(word_in_list)
+        #print(word_in_list)
         if " _ " in word_in_code:
             x = True
         else:
             x = False
-            print("Lo lograste!!! Fuiste capaz de adivinar la palabra!")
+            os.system("clear")
+            print(("Lo lograste!!! Fuiste capaz de adivinar la palabra, la cual era {choosen_word}").format(choosen_word=choosen_word))
         
 
 
